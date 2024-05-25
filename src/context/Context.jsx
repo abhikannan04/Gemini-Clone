395c70a2c7ba5f908a302a298f8297c803da8ee5
Abhilash <abhikannan200304@gmail.com>
2024-05-25 18:04:35 +0530

    Added All  module

---
+++ src/context/Context.jsx
@@ -0,0 +1,81 @@
+import { createContext, useState } from "react";
+import run from "../config/Gemini.js";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentprompt, setRecentPrompt] = useState("");
+  const [previousprompt, setPreviousPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const delayParams = (index, nextWord) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextWord);
+    }, 75 * index);
+  };
+
+   const newChat = ()=>{
+    setLoading(true);
+    setShowResult(false);
+    
+   }
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    let response;
+    if (prompt !== undefined) {
+      response = await run(prompt);
+      setRecentPrompt(prompt);
+    } else {
+      setPreviousPrompt((prev) => [...prev, input]);
+      setRecentPrompt(input);
+      response = await run(input);
+    }
+    let responseArray = response.split("**");
+    let newResponse = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextWord = newResponseArray[i];
+      delayParams(i, nextWord + " ");
+    }
+    setLoading(false);
+    setInput("");
+  };
+
+  const contextValue = {
+    input,
+    setInput,
+    recentprompt,
+    setRecentPrompt,
+    previousprompt,
+    setPreviousPrompt,
+    showResult,
+    setShowResult,
+    loading,
+    setLoading,
+    resultData,
+    setResultData,
+    onSent,
+    newChat
+  };
+
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
